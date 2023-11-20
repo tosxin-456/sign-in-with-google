82bb2f23f06f93815ea98ee4c02fcb9b49f9b56c
tosxin-456 <127943389+tosxin-456@users.noreply.github.com>
2023-11-20 08:55:27 +0100

    added all the files

---
+++ UserDtails.js
@@ -0,0 +1,9 @@
+const emailDetails ={
+  user:'amicainnovasion@gmail.com',
+  clientId:'814398620204-i74h4blksmu482t8el9ju31v9o087fji.apps.googleusercontent.com',
+  clientSecret:'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',
+  refreshToken:'1//048oWSHkaIY1SCgYIARAAGAQSNwF-L9Ir_PMYxWbiHkTa53RxGTxWIVZsfjIhSvt3eNWi3-bV-NSH_IDs1tSK2KdjTiAgbijHafg',
+  accessToken: "ya29.a0AfB_byA36oY2s4aBad71vJm9JBIq8MOnYVyrGDOj6gdWeg4BqF2F2teFj79tm-ly9Sj8tMhaOwsgiYSnZ54IXzQ-mFkNwhZvgIop-UyqpjqNWEO79VssPdWPDLGWkYwkZgVqZ_f3aIAfqyiFqf1DXKOPPWcUO8C81waCgYKAVMSARMSFQGOcNnCVq2ggBsL5GnaoxJkg131mQ0169",
+  URI: 'https://developers.google/playground'
+}
+module.exports = emailDetails
---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const userDets = require('./UserDtails')
+const passport = require("passport")
+const GoogleStrategy = require("passport-google-oauth20").Strategy
+const session = require("express-session")
+const accessToken = userDets.accessToken
+const refreshToken = userDets.refreshToken
+const app = express()
+
+app.use(session({
+  secret: 'ysgwyedygywu',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize())
+app.use(passport.session());
+
+passport.use(new GoogleStrategy({
+  clientID: userDets.clientId,
+  clientSecret: userDets.clientSecret,
+  callbackURL: userDets.URI
+},
+  (accessToken, refreshToken, profile, done) => {
+    return done(null, profile)
+  }))
